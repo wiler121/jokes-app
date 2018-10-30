96d3f5061b94ac53b5433e01ee094153f6028bdc
wiler121 <36072171+wiler121@users.noreply.github.com>
2018-10-30 21:45:38 +0100

    Working App

---
+++ src/main/java/mf/spring/jokesapp/controllers/JokeController.java
@@ -0,0 +1,28 @@
+package mf.spring.jokesapp.controllers;
+
+import mf.spring.jokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+        }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chuknorris" ;
+    }
+}
+
+
